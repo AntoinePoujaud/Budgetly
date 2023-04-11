25b1f4ee700b060d34821471067593701691ea6e
Antoine Poujaud <antoinepoujaud@gmail.com>
2023-04-12 00:20:27 +0200

    add files

---
+++ lib/pages/barChart.dart
@@ -0,0 +1,156 @@
+import 'package:budgetly/pages/app_colors.dart';
+import 'package:budgetly/pages/color_extensions.dart';
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class _BarChart extends StatelessWidget {
+  const _BarChart({required this.totalDepense, required this.totalRevenu});
+  final double totalDepense;
+  final double totalRevenu;
+
+  @override
+  Widget build(BuildContext context) {
+    return BarChart(
+      BarChartData(
+        barTouchData: barTouchData,
+        titlesData: titlesData,
+        borderData: borderData,
+        barGroups: barGroups,
+        gridData: FlGridData(show: false),
+        alignment: BarChartAlignment.spaceEvenly,
+        maxY: max(totalDepense, totalRevenu),
+      ),
+    );
+  }
+
+  BarTouchData get barTouchData => BarTouchData(
+        enabled: false,
+        touchTooltipData: BarTouchTooltipData(
+          tooltipBgColor: Colors.transparent,
+          tooltipPadding: EdgeInsets.zero,
+          tooltipMargin: 8,
+          getTooltipItem: (
+            BarChartGroupData group,
+            int groupIndex,
+            BarChartRodData rod,
+            int rodIndex,
+          ) {
+            return BarTooltipItem(
+              rod.toY.round().toString(),
+              const TextStyle(
+                color: AppColors.contentColorCyan,
+                fontWeight: FontWeight.bold,
+              ),
+            );
+          },
+        ),
+      );
+
+  Widget getTitles(double value, TitleMeta meta) {
+    final style = TextStyle(
+      color: AppColors.contentColorBlue.darken(20),
+      fontWeight: FontWeight.bold,
+      fontSize: 14,
+    );
+    String text = "";
+    switch (value.toInt()) {
+      case 0:
+        text = 'Dépenses';
+        break;
+      case 1:
+        text = 'Revenus';
+        break;
+    }
+    return SideTitleWidget(
+      axisSide: meta.axisSide,
+      space: 4,
+      child: Text(text, style: style),
+    );
+  }
+
+  FlTitlesData get titlesData => FlTitlesData(
+        show: true,
+        bottomTitles: AxisTitles(
+          sideTitles: SideTitles(
+            showTitles: true,
+            reservedSize: 30,
+            getTitlesWidget: getTitles,
+          ),
+        ),
+        leftTitles: AxisTitles(
+          sideTitles: SideTitles(showTitles: false),
+        ),
+        topTitles: AxisTitles(
+          sideTitles: SideTitles(showTitles: false),
+        ),
+        rightTitles: AxisTitles(
+          sideTitles: SideTitles(showTitles: false),
+        ),
+      );
+
+  FlBorderData get borderData => FlBorderData(
+        show: false,
+      );
+
+  LinearGradient get _barsGradient => LinearGradient(
+        colors: [
+          AppColors.contentColorBlue.darken(20),
+          AppColors.contentColorCyan,
+        ],
+        begin: Alignment.bottomCenter,
+        end: Alignment.topCenter,
+      );
+
+  List<BarChartGroupData> get barGroups => [
+        BarChartGroupData(
+          x: 0,
+          barRods: [
+            BarChartRodData(
+                toY: totalDepense,
+                color: Colors.red,
+                width: 30,
+                borderRadius: BorderRadius.zero
+                // gradient: _barsGradient,
+                )
+          ],
+          showingTooltipIndicators: [0],
+        ),
+        BarChartGroupData(
+          x: 1,
+          barRods: [
+            BarChartRodData(
+                toY: totalRevenu,
+                color: Colors.green,
+                width: 30,
+                borderRadius: BorderRadius.zero
+                // gradient: _barsGradient,
+                )
+          ],
+          showingTooltipIndicators: [0],
+        ),
+      ];
+}
+
+class BarChartSample3 extends StatefulWidget {
+  const BarChartSample3(
+      {Key? key, required this.totalDepense, required this.totalRevenu})
+      : super(key: key);
+
+  final double totalDepense;
+  final double totalRevenu;
+
+  @override
+  State<StatefulWidget> createState() => BarChartSample3State();
+}
+
+class BarChartSample3State extends State<BarChartSample3> {
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 1.106,
+      child: _BarChart(
+          totalDepense: widget.totalDepense, totalRevenu: widget.totalRevenu),
+    );
+  }
+}
