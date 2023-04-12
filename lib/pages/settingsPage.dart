dd1c1ba724bd90af55f2a400f32b0c8a6432ea09
Antoine Poujaud <antoinepoujaud@gmail.com>
2023-04-12 23:47:36 +0200

    aled

--- lib/pages/settingsPage.dart
+++ lib/pages/settingsPage.dart
@@ -22,8 +22,8 @@ class SettingsPageState extends State<SettingsPage> {
   double? _deviceHeight, _deviceWidth;
   double startingAmountAccount = 0;
   String currentPage = 'Paramètres';
-  // String serverUrl = 'https://moneytly.herokuapp.com';
-  String serverUrl = 'http://localhost:8081';
+  String serverUrl = 'https://moneytly.herokuapp.com';
+  // String serverUrl = 'http://localhost:8081';
 
   @override
   void initState() {
